34861a511a35b2a8e240f209585c5f5e19ee1ad5
Makoto-PC\Makoto <markdebruyn1992@gmail.com>
2017-10-31 23:33:40 +0100

    attempted class addition

--- WindowsFormsApp3/WindowsFormsApp3/ServerCode.cs
+++ WindowsFormsApp3/WindowsFormsApp3/ServerCode.cs
@@ -2,6 +2,7 @@
 using System.Text;
 using System.Net;
 using System.Net.Sockets;
+using System.Collections.Generic;
 
 namespace WindowsFormsApp3
 {
@@ -10,13 +11,23 @@ namespace WindowsFormsApp3
         // https://codeabout.wordpress.com/2011/03/06/building-a-simple-server-client-application-using-c/
 
         IPAddress ipAdress = IPAddress.Parse("127.0.0.1");
+        List<TcpListener> list = new List<TcpListener>();
+
+        public void CreateTCPListener()
+        {
+            TcpListener tcpListener = new TcpListener(ipAdress, 8000);
+            list.Add(tcpListener);
+        }
+
         public void Test(string clientIpAdress)
         {
             IPAddress ipAdress = IPAddress.Parse(clientIpAdress);
-            TcpListener myList = new TcpListener(ipAdress, 8000);
-            myList.Start();
+            
+            TcpListener tcpListener = new TcpListener(ipAdress, 8000);
+            list.Add(tcpListener);
+            tcpListener.Start();
 
-            Socket s = myList.AcceptSocket();
+            Socket s = tcpListener.AcceptSocket();
 
             // When accepted
             Console.WriteLine("Connection accepted from " + s.RemoteEndPoint);
@@ -36,7 +47,7 @@ namespace WindowsFormsApp3
             Console.WriteLine("\n Automatic message sent!");
 
             s.Close();
-            myList.Stop();
+            tcpListener.Stop();
 
         }
 
