4f79debf0b443d88e083bce14887110aeca0f098
Makoto-PC\Makoto <markdebruyn1992@gmail.com>
2017-11-01 19:53:10 +0100

    again...

--- WindowsFormsApp3/WindowsFormsApp3/server.cs
+++ WindowsFormsApp3/WindowsFormsApp3/server.cs
@@ -37,7 +37,7 @@ namespace WindowsFormsApp3
 
         public void ReadData(ClientInfo ci, String text)
         {
-            MessageBox.Show("WORKING?");
+            //MessageBox.Show("WORKING?");
             Console.WriteLine("Received from " + ci.ID + ": " + text);
             if (text[0] == '!')
                 server.Broadcast(Encoding.UTF8.GetBytes(text));
